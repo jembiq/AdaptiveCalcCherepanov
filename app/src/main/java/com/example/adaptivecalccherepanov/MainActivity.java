a085e1ebdac42a58c87ed2274b611fa251dde293
jembiq <vadim.vadim4@yandex.ru>
2019-12-28 14:47:08 +0300

    mod

--- app/src/main/java/com/example/adaptivecalccherepanov/MainActivity.java
+++ app/src/main/java/com/example/adaptivecalccherepanov/MainActivity.java
@@ -9,13 +9,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static TextView mSwitchModeBtn;
-    private static FrameLayout mEngineerBtns;
+    TextView mSwitchModeBtn;
+    FrameLayout mEngineerBtns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
 
         init();
